d701e3ada93d7947e23b894de020d2684af85dc3
X-Green <2271174177@qq.com>
2021-01-02 01:32:21 +0800

    Config IO

--- src/main/java/dev/eeasee/custom_skybox/CustomSkyBoxMod.java
+++ src/main/java/dev/eeasee/custom_skybox/CustomSkyBoxMod.java
@@ -1,13 +1,13 @@
 package dev.eeasee.custom_skybox;
 
 import dev.eeasee.custom_skybox.configs.ConfigHolder;
-import dev.eeasee.custom_skybox.configs.ConfigReader;
+import dev.eeasee.custom_skybox.configs.ConfigIO;
 import net.fabricmc.api.ModInitializer;
 
 public class CustomSkyBoxMod implements ModInitializer {
     public static ConfigHolder configs;
     @Override
     public void onInitialize() {
-        configs = ConfigReader.readConfigFromFile();
+        configs = ConfigIO.readConfigFromFile();
     }
 }
---
+++ src/main/java/dev/eeasee/custom_skybox/configs/ConfigIO.java
@@ -0,0 +1,52 @@
+package dev.eeasee.custom_skybox.configs;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.*;
+
+public class ConfigIO {
+    private static final File CONFIG_FILE = new File("config/eeasee-custom-skybox.json");
+
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    private static ConfigHolder readConfigsFromFile(File configFile) {
+        ConfigHolder config;
+        try {
+            config = GSON.fromJson(new FileReader(configFile), ConfigHolder.class);
+        } catch (FileNotFoundException | JsonSyntaxException | JsonIOException e) {
+            config = new ConfigHolder();
+        }
+        return config;
+    }
+
+    public static ConfigHolder readConfigFromFile() {
+        return readConfigsFromFile(CONFIG_FILE);
+    }
+
+    private static void writeConfigToFile(ConfigHolder configs, File configFile) {
+        configFile.getParentFile().mkdirs();
+        String output = GSON.toJson(configs);
+
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = new FileOutputStream(configFile);
+            outputStream.write(output.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeConfigToFile(ConfigHolder configs) {
+        writeConfigToFile(configs, CONFIG_FILE);
+    }
+
+}
